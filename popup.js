7581be93509dac848ef644fdfeb29ae95270edde
Jun Pataleta <jun@moodle.com>
2018-05-09 12:16:14 +0800

    Various fixes!
    * Dynamically generate pull buttons.
    * Use Bootstrap 4.

--- popup.js
+++ popup.js
@@ -1,9 +1,11 @@
-var pullBranches = [];
-var pullFromRepository = '';
+let pullBranches = [];
+let pullFromRepository = '';
 
 chrome.extension.onRequest.addListener(function(links) {
-    for (var index in links) {
-        var container = $(links[index]);
+    // Reverse the links so master comes first, then latest stable down to oldest.
+    links.reverse();
+    for (let index in links) {
+        let container = $(links[index]);
         if (container.text().indexOf('Pull') >= 0) {
             if (container.text().indexOf('Diff URL') >= 0) {
                 continue;
@@ -11,9 +13,9 @@ chrome.extension.onRequest.addListener(function(links) {
             if (container.text().indexOf('from Repository') >= 0) {
                 pullFromRepository = $(container[2]).text().trim();
             } else {
-                var parts = $(container[0]).text().split(" ");
-                var version = null;
-                for (var i in parts) {
+                let parts = $(container[0]).text().split(" ");
+                let version = null;
+                for (let i in parts) {
                     if (parts[i].length > 0 && parts[i] !== 'Pull' && parts[i] !== 'Branch:') {
                         version = parts[i].toLowerCase();
                         version = version.replace(".", "");
@@ -21,8 +23,22 @@ chrome.extension.onRequest.addListener(function(links) {
                 }
                 if (version !== null) {
                     pullBranches[version] = $(container[2]).text().trim();
-                    // Enable this copy button.
-                    $("#copy-" + version).removeAttr('disabled');
+                    let versionText = version;
+                    if (version !== 'master') {
+                        versionText = 'MOODLE_' + version + '_STABLE';
+                    }
+                    let buttonDiv = $('<div class="form-group text-center"/>');
+                    let buttonHtml = '<button class="btn btn-default btn-block" id="copy-' + version +
+                        '" data-version="' + version + '">' + versionText + '</button>';
+                    let button = $(buttonHtml);
+                    buttonDiv.html(button);
+                    // Append this button to the form.
+                    buttonDiv.appendTo('#pull-form');
+                    // Add event listener to button.
+                    button.click(function(e) {
+                        e.preventDefault();
+                        generatePullCommand(this);
+                    });
                 }
             }
         }
@@ -30,23 +46,19 @@ chrome.extension.onRequest.addListener(function(links) {
 });
 
 function generatePullCommand(button) {
-    var version = $(button).data('version');
-    var branch = $(button).text();
-    var result = 'git checkout ' + branch + ' && git pull ' + pullFromRepository + ' ' + pullBranches[version];
-    $("#git-command").val(result);
-    $("#git-command").focus();
+    let version = $(button).data('version');
+    let branch = $(button).text();
+    let result = 'git checkout ' + branch + ' && git pull ' + pullFromRepository + ' ' + pullBranches[version];
+    let commandText = $("#git-command");
+    commandText.val(result);
+    commandText.focus();
     document.execCommand('SelectAll');
     document.execCommand("Copy", false, null);
     console.log("Git pull command '" + result + "' has been copied to the clipboard.");
-    window.close()
+    window.close();
 }
 
 $(document).ready(function() {
-    $('button').click(function(e) {
-        e.preventDefault();
-        generatePullCommand(this);
-    });
-
     chrome.windows.getCurrent(function (currentWindow) {
         chrome.tabs.query({active: true, windowId: currentWindow.id},
             function(activeTabs) {
